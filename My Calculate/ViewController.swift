56084d5fd4982c43fedea31bd80f2c9f9e7827f1
student14 <chaidan@msn.com>
2017-06-13 10:25:59 +0700

    Print to Log OK

--- My Calculate/ViewController.swift
+++ My Calculate/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+// Declare variable area.(Hold Control botton with right drag)
+// Implicit
+    var intNumber1 = 0
+    var intNumber2: Int = 0
+    var strNumber1 = ""
+    var strNumber2: String = ""
+    
+    
+    
+    
+    @IBOutlet weak var num1TextField: UITextField!
+    
+    @IBOutlet weak var num2TextField: UITextField!
+    
+    @IBAction func answerButton(_ sender: Any) {
+            strNumber1 = num1TextField.text!
+            strNumber2 = num2TextField.text!
+            // show on Log
+            print("strNumber1 is \(strNumber1)")
+            print("strNumber2 is \(strNumber2)")
+    } // answerButton
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    } // main method : the viewDidLoad is the method will be started first.
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    } // memory method : Memory management
 
 
-}
+} // main class
 
